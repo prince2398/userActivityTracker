102caeadd39f4e4850fde3cc39698052dadc3273
prince2398 <princepiyush2398@gmail.com>
2018-10-14 11:31:18 +0530

    Node api added to get and update user data

---
+++ routes/index.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const router = express.Router();
+const mongoose = require('mongoose');
+
+//database
+mongoose.connect('mongodb://prince:Prince2398@ds131753.mlab.com:31753/userdata',{ useNewUrlParser: true });
+mongoose.connection.on('connected',()=>{
+    console.log("Connected to DB");
+});
+const UserSchema = mongoose.Schema({
+    clicks : [Number],
+    hovers : [Number]
+})
+const User = mongoose.model('User',UserSchema);
+
+//routes
+router.get('/:id',(req,res)=>{
+    console.log(req.params.id);
+    User.findById(req.params.id, (err,user)=>{
+        if(err){
+            res.send(err);
+        }
+        res.json(user);
+    })
+});
+router.get('/',(req,res,next)=>{
+    var newUser = new User({
+        clicks : [0,0,0,0,0],
+        hovers : [0,0,0,0,0]
+    });
+    newUser.save((err,user)=>{
+        if(err){
+            res.send(err);
+        }
+        res.redirect('/'+user._id);
+    })
+})
+router.put('/:id',(req,res)=>{
+    console.log(req.body);
+    if(!req.body){
+        res.send('Bad Data');
+    }
+    User.findByIdAndUpdate(req.params.id,{
+        clicks : req.body.clicks,
+        hovers : req.body.hovers 
+    },{new :true}, (err,user)=>{
+        if(err){
+            res.send('error Updating');
+        }
+        res.json(user);
+    })
+})
+router.get('/:id/admin',(req,res)=>{
+    console.log(req.params.id);
+    User.findById(req.params.id, (err,user)=>{
+        if(err){
+            res.send(err);
+        }
+        res.json(user);
+    })
+})
+module.exports = router;
--- server.js
+++ server.js
@@ -1,28 +1,28 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-
-mongoose.connect('mongodb://localhost/test');
-
-const index = require('./routes/index')
-const admin = require('./routes/admin')
-
-const port = 3000;
 
 const app = express();
 
+const port = 3000;
+
+//routes
+const index = require('./routes/index')
+
+//views
 app.set('views',path.join(__dirname,'views'));
 app.set('view engine','ejs');
 app.engine('html',require('ejs').renderFile);
 
+//body-parser
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true })); 
 
+//static files
 app.use(express.static(__dirname + '/public')); 
 
+//route
 app.use('/',index);
-app.use('/api',admin);
 
 app.listen(port,()=>{
     console.log(`Server started on port ${port}`);
